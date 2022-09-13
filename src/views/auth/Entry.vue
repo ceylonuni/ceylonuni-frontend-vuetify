7a9005ba678cc4090c41cf9976b69db58970d4d5
josiahprathaban <josiah.prathaban@gmail.com>
2022-09-13 23:19:57 +0530

    login api integrated

--- src/views/auth/Entry.vue
+++ src/views/auth/Entry.vue
@@ -12,7 +12,8 @@
           <v-card-text>
             <v-text-field
               label="Email"
-              value="john@vuetifyjs.com"
+              placeholder="john@vuetifyjs.com"
+              v-model="email"
             ></v-text-field>
             <span class="text-caption grey--text text--darken-1">
               This is the email you will use to login to your Vuetify account
@@ -22,7 +23,11 @@
 
         <v-window-item :value="2">
           <v-card-text>
-            <v-text-field label="Password" type="password"></v-text-field>
+            <v-text-field
+              label="Password"
+              type="password"
+              v-model="password"
+            ></v-text-field>
             <span class="text-caption grey--text text--darken-1">
               Please enter a password for your account
             </span>
@@ -31,14 +36,29 @@
 
         <v-window-item :value="3">
           <div class="pa-4 text-center">
-            <v-img
-              class="mb-4"
-              contain
-              height="128"
-              src="https://cdn.vuetifyjs.com/images/logos/v.svg"
-            ></v-img>
-            <h3 class="text-h6 font-weight-light mb-2">Welcome to Vuetify</h3>
-            <span class="text-caption grey--text">Thanks for signing up!</span>
+            <!-- <h3 class="text-h6 font-weight-light mb-2">Email not registred!</h3> -->
+            <span class="text-caption grey--text">
+              The email you entred is not registred in the system. Do you want
+              to register?
+            </span>
+          </div>
+        </v-window-item>
+        <v-window-item :value="4">
+          <div class="pa-4 text-center">
+            <!-- <h3 class="text-h6 font-weight-light mb-2">Email Not Valid!</h3> -->
+            <span class="text-caption grey--text">
+              The email you entered is not a valid university email address.
+              Please try with an valid email.
+            </span>
+          </div>
+        </v-window-item>
+        <v-window-item :value="5">
+          <div class="pa-4 text-center">
+            <!-- <h3 class="text-h6 font-weight-light mb-2">Email Not Valid!</h3> -->
+            <span class="text-caption grey--text">
+              A verification link has been sent to your mail. Click the link to
+              continue your registration.
+            </span>
           </div>
         </v-window-item>
       </v-window>
@@ -46,10 +66,15 @@
       <v-divider></v-divider>
 
       <v-card-actions>
-        <v-btn :disabled="step === 1" text @click="step--"> Back </v-btn>
+        <v-btn :disabled="step === 1" text @click="step = 1"> Back </v-btn>
         <v-spacer></v-spacer>
-        <v-btn :disabled="step === 3" color="primary" depressed @click="step++">
-          Next
+        <v-btn
+          @click="verifyEmail"
+          :disabled="step === 4 || step === 5"
+          color="primary"
+          depressed
+        >
+          {{ step == 3 ? "Confirm" : "Next" }}
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -57,6 +82,7 @@
 </template>
 <!-- eslint-disable prettier/prettier -->
 <script>
+const axios = require("axios").default;
 export default {
   components: {
     Welcome: () =>
@@ -65,6 +91,8 @@ export default {
       ),
   },
   data: () => ({
+    email: null,
+    password: null,
     step: 1,
   }),
 
@@ -72,11 +100,65 @@ export default {
     currentTitle() {
       switch (this.step) {
         case 1:
-          return "Sign-up";
+          return "Login";
         case 2:
-          return "Create a password";
+          return "Enter password";
+        case 3:
+          return "Confirm register";
+        case 5:
+          return "Verify";
         default:
-          return "Account created";
+          return "Invalid email";
+      }
+    },
+  },
+  methods: {
+    verifyEmail() {
+      if (this.step == 1) {
+        // verify email
+        axios
+          .post("http://localhost:3000/api/auth/mail/verify", {
+            email: this.email,
+          })
+          .then((response) => {
+            console.log(response);
+            if (response.data.existing) {
+              this.step = 2;
+            } else if (response.data.vaild) {
+              this.step = 3;
+            } else {
+              this.step = 4;
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else if (this.step == 2) {
+        // login email password
+        axios
+          .post("http://localhost:3000/api/auth/login", {
+            email: this.email,
+            password: this.password,
+          })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else if (this.step == 3) {
+        // send verification email
+        axios
+          .post("http://localhost:3000/api/auth/mail/send", {
+            email: this.email,
+          })
+          .then((response) => {
+            console.log(response);
+            this.step = 5;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       }
     },
   },
